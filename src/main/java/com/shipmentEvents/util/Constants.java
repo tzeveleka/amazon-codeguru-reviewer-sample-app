f2fb63c667d017050fa8c6407364de29f053b6c2
Anastasia Tzeveleka <atzeveleka@gmail.com>
2021-07-21 18:51:12 +0100

    changed yml

--- src/main/java/com/shipmentEvents/util/Constants.java
+++ src/main/java/com/shipmentEvents/util/Constants.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public final class Constants {
-
     public static final List<String> BUCKETS_TO_PROCESS = Arrays.asList("shipment-events-from-scanner", "shipment-events-from-mobile-app");
     public static final String SUMMARY_BUCKET = "shipment-events-summary"; 
 }
